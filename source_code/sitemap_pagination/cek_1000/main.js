498898735ec2f8421980a2bd94780262c46cbd17
siwaluh <dayatsiwaluh@gmail.com>
2024-10-04 15:34:11 +0800

    x

--- source_code/sitemap_pagination/cek_1000/main.js
+++ source_code/sitemap_pagination/cek_1000/main.js
@@ -1,59 +1,59 @@
 let postSitemap = new BloggerSitemap({
-  'firstContent': true,
-  'host': 'www.novelturk.com',
-  'label': 'Reincarnation Of The Strongest Sword God'
-}),
-sitemapElement = document.getElementById('sitemap_navigation'),
-customPostElement = document.getElementById('custom_post_v1'),
-searchParams = new URLSearchParams(document.location.search),
-maxResults = searchParams.get('max-results') || 5,
-postSitemapPaginationArr,
-createNavigation,
-navigationElement;
+    'firstContent': true,
+    'host': 'www.novelturk.com',
+    'label': 'Reincarnation Of The Strongest Sword God'
+  }),
+  sitemapElement = document.getElementById('sitemap_navigation'),
+  customPostElement = document.getElementById('custom_post_v1'),
+  searchParams = new URLSearchParams(document.location.search),
+  maxResults = searchParams.get('max-results') || 5,
+  postSitemapPaginationArr,
+  createNavigation,
+  navigationElement;
 
 postSitemap.run(({
-posts: entry,
-totalGet,
-completed: isLast,
-totalPosts: totalResults
+  posts: entry,
+  totalGet: totalGet,
+  completed: isLast,
+  totalPosts: totalResults
 }) => {
-if (totalGet == 1 && entry.length != 0) {
-  let firstPosts = entry.slice(0, maxResults);
+  if (!totalGet && entry.length != 0) {
+    let firstPosts = entry.slice(0, maxResults);
 
-  //Create Custom Post
-  bloggerHtml.custom_post_v1(firstPosts, customPostElement);
+    //Create Custom Post
+    bloggerHtml.custom_post_v1(firstPosts, customPostElement);
 
-  //createPagination
-  createNavigation = new Pagination({
-    'callback': function (mainElement, currentPage) {
-      if (mainElement.innerHTML == '')
-        return;
+    //createPagination
+    createNavigation = new Pagination({
+      'callback': function (mainElement, currentPage) {
+        if (mainElement.innerHTML == '')
+          return;
 
-      if (postSitemapPaginationArr || currentPage == 1) {
+        if (postSitemapPaginationArr || currentPage == 1) {
 
-        //Remove Pagination Element
-        if (navigationElement && 'mainElement' in navigationElement)
-          navigationElement.mainElement.remove();
+          //Remove Pagination Element
+          if (navigationElement && 'mainElement' in navigationElement)
+            navigationElement.mainElement.remove();
 
-        //create new Pagination and postList
-        navigationElement = {
-          mainElement,
-          currentPage
-        };
-        sitemapElement.appendChild(mainElement);
-        if (postSitemapPaginationArr) {
-          bloggerHtml.custom_post_v1(postSitemapPaginationArr[currentPage - 1].items, customPostElement);
+          //create new Pagination and postList
+          navigationElement = {
+            mainElement,
+            currentPage
+          };
+          sitemapElement.appendChild(mainElement);
+          if (postSitemapPaginationArr) {
+            bloggerHtml.custom_post_v1(postSitemapPaginationArr[currentPage - 1].items, customPostElement);
+          }
+
+        } else {
+          alert('Error: Sitemap Belum selesai dibuat.');
         }
-
-      } else {
-        alert('Error: Sitemap Belum selesai dibuat.');
       }
-    }
-  });
-  createNavigation.arrayToPage(totalResults, maxResults, true);
-}
+    });
+    createNavigation.arrayToPage(totalResults, maxResults, true);
+  }
 
-if (isLast) {
-  postSitemapPaginationArr = createNavigation.arrayToPage(entry, maxResults);
-}
+  if (isLast) {
+    postSitemapPaginationArr = createNavigation.arrayToPage(entry, maxResults);
+  }
 }, false);
