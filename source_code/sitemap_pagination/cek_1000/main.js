73a9580b58e0da7c444108a5fdbea8cdec0f9025
siwaluh <dayatsiwaluh@gmail.com>
2024-10-04 15:11:56 +0800

    Pagination_cek_1000_post

---
+++ source_code/sitemap_pagination/cek_1000/main.js
@@ -0,0 +1,59 @@
+let postSitemap = new BloggerSitemap({
+  'firstContent': true,
+  'host': 'Reincarnation Of The Strongest Sword God',
+  'label': 'Series'
+}),
+sitemapElement = document.getElementById('sitemap_navigation'),
+customPostElement = document.getElementById('custom_post_v1'),
+searchParams = new URLSearchParams(document.location.search),
+maxResults = searchParams.get('max-results') || 5,
+postSitemapPaginationArr,
+createNavigation,
+navigationElement;
+
+postSitemap.run(({
+posts: entry,
+totalGet,
+completed: isLast,
+totalPosts: totalResults
+}) => {
+if (totalGet == 1 && entry.length != 0) {
+  let firstPosts = entry.slice(0, maxResults);
+
+  //Create Custom Post
+  bloggerHtml.custom_post_v1(firstPosts, customPostElement);
+
+  //createPagination
+  createNavigation = new Pagination({
+    'callback': function (mainElement, currentPage) {
+      if (mainElement.innerHTML == '')
+        return;
+
+      if (postSitemapPaginationArr || currentPage == 1) {
+
+        //Remove Pagination Element
+        if (navigationElement && 'mainElement' in navigationElement)
+          navigationElement.mainElement.remove();
+
+        //create new Pagination and postList
+        navigationElement = {
+          mainElement,
+          currentPage
+        };
+        sitemapElement.appendChild(mainElement);
+        if (postSitemapPaginationArr) {
+          bloggerHtml.custom_post_v1(postSitemapPaginationArr[currentPage - 1].items, customPostElement);
+        }
+
+      } else {
+        alert('Error: Sitemap Belum selesai dibuat.');
+      }
+    }
+  });
+  createNavigation.arrayToPage(totalResults, maxResults, true);
+}
+
+if (isLast) {
+  postSitemapPaginationArr = createNavigation.arrayToPage(entry, maxResults);
+}
+}, false);
